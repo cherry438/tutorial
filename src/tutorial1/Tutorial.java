38436f82ade0c05609e401800098f77a2946c6ed
Cherry Kim <cherry3880@163.com>
2020-10-02 16:00:34 +0900

    Add test comment

--- src/tutorial1/Tutorial.java
+++ src/tutorial1/Tutorial.java
@@ -4,6 +4,12 @@ public class Tutorial {
     public static void main(String[] args) {
         System.out.println("안녕 세상");
         int n = 0;
+        // 아래 코드는 두개의 숫자를 더하는 기능입니다.
+        /*
+            이것도
+            저것도
+            요것도
+         */
         n += addNumber(n, 5);
         System.out.println(n);
     }
